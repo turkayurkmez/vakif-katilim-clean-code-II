667df880c66070b834e61a3aa6816b3eda85452f
türkay ürkmez <turkay.urkmez@outlook.com>
2024-04-25 14:04:48 +0300

    Talking about ISP

---
+++ SOLID/LiskovsSubstution/Geometry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovsSubstution
+{
+    public class Geometry
+    {
+        public ICalcutableArea GetRectangle(int unit1, int? unit2=null)
+        {
+            if (unit2.HasValue)
+            {
+                return new Rectangle { Width = unit1, Height = unit2.Value };
+            }
+
+
+            return new Square() {  EdgeLength=unit1};
+        }
+    }
+
+    public interface ICalcutableArea
+    {
+        double GetArea();
+    }
+
+    public class Rectangle : ICalcutableArea
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public double GetArea() => Width * Height;
+    }
+
+    public class Square : ICalcutableArea//: Rectangle
+    {
+        public int EdgeLength { get; set; }
+        public double GetArea() => Math.Pow(EdgeLength, 2);
+    }
+
+}
--- SOLID/LiskovsSubstution/Program.cs
+++ SOLID/LiskovsSubstution/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LiskovsSubstution;
+
 Console.WriteLine("Hello, World!");
+var rectangle = new Geometry().GetRectangle(10);
+
+Console.WriteLine( rectangle.GetArea());
