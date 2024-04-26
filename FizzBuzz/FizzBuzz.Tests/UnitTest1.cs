4614c4f2a5da2d91881115998f8261265e1e8c8f
türkay ürkmez <turkay.urkmez@outlook.com>
2024-04-26 14:46:01 +0300

    FizzBuzz ant test process

---
+++ FizzBuzz/FizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿namespace FizzBuzz.Tests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void IsExists()
+        //{
+        //    /*
+        //     * Nesne ihtiyaçları
+        //     */
+        //    var game = new FizzBuzz.Game();
+        //    string output = game.GetWord(3);
+        //}
+
+        [Fact]
+        public void When_send_3_then_return_Fizz()
+        {
+            //AAA
+            //Arrange
+            var game = new Game();
+            var value = 3;
+            //Act
+            string output = game.GetWord(value);
+
+            //Assert
+            Assert.Equal(output, "Fizz");
+        }
+
+        [Fact]
+        public void When_Send_5_then_return_buzz()
+        {
+            var game = new Game();
+            var value = 5;
+            string output = game.GetWord(value);
+            Assert.Equal("Buzz", output);
+        }
+
+        [Theory]
+        [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(12)]
+        public void when_send_multiply_three_return_Fizz(int value)
+        {
+            var game = new Game();
+            string actual = game.GetWord(value);
+
+            Assert.Equal("Fizz", actual);
+        }
+
+
+        [Theory]
+        [InlineData(10)]
+        [InlineData(25)]
+        [InlineData(20)]
+        public void when_send_multiply_five_return_Fizz(int value)
+        {
+            var game = new Game();
+            string actual = game.GetWord(value);
+
+            Assert.Equal("Buzz", actual);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        public void when_send_multiply_fifteen_return_Fizz(int value)
+        {
+            var game = new Game();
+            string actual = game.GetWord(value);
+
+            Assert.Equal("FizzBuzz", actual);
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void when_send_literally_numbers_return_number_string(int value)
+        {
+            var game = new Game();
+            string actual = game.GetWord(value);
+            Assert.Equal(value.ToString(), actual);
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Game
+    {
+        public string GetWord(int value)
+        {
+            if (value % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (value % 3==0)
+            {
+                return "Fizz";
+            }
+            else if(value % 5 ==0)
+            {
+                return "Buzz";
+            }
+            return value.ToString();
+        }
+    }
+}
